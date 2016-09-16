d39bd0b237dab1345a04a9770e58eb9f198978ac
livegameshub <livegameshub@gmail.com>
2016-09-16 10:11:42 +0300

    More work on the BasicResource class

--- Projects/Engine/Source/BasicResource.cpp
+++ Projects/Engine/Source/BasicResource.cpp
@@ -4,10 +4,61 @@ namespace ai
 {
 	BasicResource::BasicResource(u32 id)
 		: BasicElement(id)
+		, mReferencesCounter(0)
 	{
 	}
 
 	BasicResource::~BasicResource()
 	{
+	}
+
+	bool BasicResource::Load()
+	{
+		if (!mReferencesCounter)
+		{
+			if (!Create())
+			{
+				return false;
+			}
+		}
+
+		++mReferencesCounter;
+
+		return true;
+	}
+
+	bool BasicResource::Unload()
+	{
+		if (!mReferencesCounter)
+		{
+			return false;
+		}
+
+		if (mReferencesCounter == 1)
+		{
+			if (!Release())
+			{
+				return false;
+			}
+		}
+
+		--mReferencesCounter;
+
+		return true;
+	}
+
+	void BasicResource::SetResourceFile(const std::string& file)
+	{
+		mResourceFile = file;
+	}
+
+	std::string BasicResource::GetResourceFile() const
+	{
+		return mResourceFile;
+	}
+
+	u32 BasicResource::GetReferencesCounter() const
+	{
+		return mReferencesCounter;
 	}
 }
--- Projects/Engine/Source/BasicResource.h
+++ Projects/Engine/Source/BasicResource.h
@@ -5,20 +5,26 @@
 
 namespace ai
 {
-	class ENGINE_API BasicResource : public BasicElement
+	class BasicResource : public BasicElement
 	{
 	public:
-		BasicResource(u32 id);
+		ENGINE_API BasicResource(u32 id);
 		
-		virtual ~BasicResource();
+		ENGINE_API virtual ~BasicResource();
 
-		bool Load();
-		bool Unload();
+		ENGINE_API bool Load();
+		ENGINE_API bool Unload();
+
+		ENGINE_API inline u32 GetReferencesCounter() const;
+
+		ENGINE_API inline std::string GetResourceFile() const;
+		ENGINE_API inline void SetResourceFile(const std::string& file);
 
 	private:
 		virtual bool Create() = 0;
 		virtual bool Release() = 0;
 
+		std::string mResourceFile;
 		u32 mReferencesCounter;
 	};
 }
